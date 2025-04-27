d981d9b1a16a0ccdf6f2836f02d7cca9b88f33c1
Enzo de Freitas <enzo.freitas@sptech.com>
2025-04-26 22:59:23 -0300

    Inicialização do projeto. Efeito parallax em home e tentativa de inserção da biblioteca locomotive-scroll.

---
+++ js/script_locomotive.js
@@ -0,0 +1,10 @@
+// Inicialização da biblioteca
+const scroll = new LocomotiveScroll({
+     /* el significa em qual elemento inicializar o locomotive scroll 
+        Irá procurar pelo 'data-scroll-container', por isso é preciso mapeá-lo
+    */
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+   
+        
