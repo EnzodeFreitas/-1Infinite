d89096dc01f5d29d699d598f15bd77ebe58b2b14
Enzo de Freitas <enzo.freitas@sptech.com>
2025-05-03 22:46:01 -0300

    Criação da tela de login e cadastro, bem como reajustes no index e remoção de imagens

--- js/parallax.js
+++ js/parallax.js
@@ -5,26 +5,21 @@
     let estrada = document.getElementById("estrada");
     let texto = document.getElementById("texto");
 
-   scroll.on('scroll', (args) => { /* Normal seria usar 'window.addEventListener', porém a biblioteca do locomotive 
-                                      scroll iria anular. Por isso, usar comando do sistema interno locomotive*/
-        let value = args.scroll.y; // valor do scroll. Quando multiplicado, irá controlar a velocidade do movimento
-
-        bg.style.top = -value * 0.5 + 'px'; // Move para cima, mais devagar que o scroll (efeito de profundidade).
-        texto.style.left = value * 1 + 'px'; // Movimentação na mesma velocidade do scroll
-        lua.style.left = -value * 0.5 + 'px'; // O sinal de - faz a lua ir pro lado oposto.
-        montanha.style.top = value * 0.15 + 'px'; // Move as montanhas bem devagar pra baixo 
-        montanha_direita.style.top=  value * 0.15 + 'px'; // Camada para esconder o texto (mesma velocidade que motanha original) 
-        
-
         /*
             A posição da variável irá mudar de acordo com o valor de 'top' ou 'left'. 
             Como o valor dessas duas propriedades é calculado com base no valor do scroll, 
             as variáveis (que são imagens) irão se mover conforme o usuário rola a página. 
-
-            px é a unidade de medida para as
         */
 
-        
 
+   scroll.on('scroll', (args) => { /* Normal seria usar 'window.addEventListener', porém a biblioteca do locomotive 
+                                      scroll iria anular. Por isso, usar comando do sistema interno locomotive*/
+        let value = args.scroll.y; // valor do scroll. Quando multiplicado, irá controlar a velocidade do movimento
 
+        /* O sinal de - faz o elemento ir ao lado oposto. */
+        bg.style.top = -value * 0.5 + 'px'; // Move para cima, mais devagar que o scroll (efeito de profundidade). 
+        texto.style.left = value * 1 + 'px'; // Movimentação na mesma velocidade do scroll
+        lua.style.left = `calc(75% - ${value * 0.5}px)`; // Em relação à posição css do id: lua
+        montanha.style.top = value * 0.15 + 'px'; // Move as montanhas bem devagar pra baixo 
+        montanha_direita.style.top=  value * 0.15 + 'px'; // Camada para esconder o texto (mesma velocidade que motanha original) 
     })
