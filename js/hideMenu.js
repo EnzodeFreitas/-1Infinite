6257f07352efecd4c2d6bf94c639d03cb7c253e1
Enzo de Freitas <enzo.freitas@sptech.com>
2025-05-06 23:21:19 -0300

    melhora da navbar e informacções do lado esquerdo da tela, além de estruturar as divs de melhor fomra com flexbox e alteração da section princpal

---
+++ js/hideMenu.js
@@ -0,0 +1,18 @@
+function hideMenu() {
+    var menu = document.querySelector(".menu");
+    var social = document.querySelector(".social");
+    var menuIcon = document.getElementById("menu-icon");
+
+    menu.classList.toggle("hidden");
+    social.classList.toggle("hidden");
+
+ 
+ // Alternar o ícone
+ if (menu.classList.contains("hidden")) {
+     menuIcon.classList.remove("bx-x");
+     menuIcon.classList.add("bx-menu");
+ } else {
+     menuIcon.classList.remove("bx-menu");
+     menuIcon.classList.add("bx-x");
+ }
+}
