022af681d53b13b8556968f73df6405cca389240
EnzodeFreitas <enzo.freitas@sptech.school>
2025-05-29 14:47:06 -0300

    Atualização do caminho para dados da dashboard

---
+++ src/controllers/dashboardController.js
@@ -0,0 +1,13 @@
+var dashboardModel = require("../models/dashboardModel");
+
+function salvarRota(req, res){
+    var geo = req.body.geojson;
+    var distancia = req.body.distancia_km
+    dashboardModel.
+};
+
+
+modele.exports = {
+    salvarRota,
+
+}
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+var dashboardController = require("../controllers/dashboardController");
 router.post("/api/salvar-forma", (req, res) => {
     const { geojson, tipo, descricao, id } = req.body;
 
@@ -11,9 +11,11 @@ router.post("/api/salvar-forma", (req, res) => {
 
 router.post("/api/salvar-rota", (req, res) => {
     const { geojson, distancia_km, descricao, id } = req.body;
+    dashboardController.salvarRota(req, res)
+
 
     // Aqui você salva a rota no banco
-    console.log("Rota recebida:", geojson, distancia_km);
+    console.log("Rota recebida:", geojson, distancia_km, );
 
     res.json({ sucesso: true, mensagem: "Rota salva com sucesso!" });
 });
